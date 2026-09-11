f9289b7cf69c3ede316bc6d9ab0f67f41cfa0027
LamaHaji <lamaalhaji235@gmail.com>
2026-09-11 08:19:54 -0700

    Use dynamic port for hosting

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 // تشغيل السيرفر
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-    console.log('Visit: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
